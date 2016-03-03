2b69625ed0736ba0ebcf93bddc24df627c54213d
somtoeluwa <1412632@rgu.ac.uk>
2016-03-03 12:09:22 +0000

    Nes styles

--- new_host.php
+++ new_host.php
@@ -5,8 +5,8 @@
     <title>FriendshipLinkWebApp</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
-    <link rel="stylesheet" href="css/unsemantic-grid-responsive-tablet.css">
-    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"/>
+    <!--<link rel="stylesheet" href="css/unsemantic-grid-responsive-tablet.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"/>-->
 
 </head>
 <body>

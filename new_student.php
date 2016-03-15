7b2c4bb9f69e1a6a5ebec0761612c25f88552378
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-15 14:15:54 +0000

    edits for screen shot

--- new_student.php
+++ new_student.php
@@ -102,14 +102,10 @@
             <input required type="radio" id="status" name="status" value="Single"/>Single
             <br>
             <br>
-            <?php if($_GET['status'] == "Married") {
-
-                echo '<label for="ifMarried">If married, Enter details(if not applicable enter null):</label>
+            <label for="ifMarried">If married, Enter details(if not applicable enter null):</label>
             <textarea required name="ifMarried" id="ifMarried" cols="45" rows="5" value=""></textarea>
             <br>
-            <br>';
-            }
-            ?>
+            <br>
             <label for="university">University:</label>
             <input required type="radio" id="university" name="university" value="RGU"/>Robert Gordon
             <input required type="radio" id="university" name="university" value="Aberdeen University"/>Aberdeen University

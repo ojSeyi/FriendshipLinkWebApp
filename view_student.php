e653116475d35682a44efdc3e34a8f0050edf0c9
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 15:54:04 +0000

    new Db connection new viewstudent page

--- view_student.php
+++ view_student.php
@@ -1,4 +1,88 @@
-<?php
+<?php include 'dbConnect.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
+</head>
+<body>
+<header>
+    <div id ="logo">
+        <a href="index.html"><img src="images/fi-logo.png" alt="Demo" width="200"></a>
+    </div>
+
+    <h1><a href="index.html">FriendshipLink WebApp</a></h1>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li class="dropdown">
+                <a class="active dropbtn" href="#">New</a>
+                <div class="dropdown-content">
+                    <a href="new_student.php">New Student Details</a>
+                    <a href="new_host.php">New Host Details</a>
+                </div>
+            </li>
+            <li  class="dropdown">
+                <a href="#" class="dropbtn">View</a>
+                <div class="dropdown-content">
+                    <a href="view_student.php">View Students </a>
+                    <a href="#">View Hosts</a>
+                    <a href="#">View Match</a>
+                </div>
+            </li>
+            <li><a href="#">Create Match</a></li>
+            <li><a href="#">Generate Report</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+<h2>Student Table</h2>
+
+
+    <table style="margin-left: auto; margin-right: auto;">
+        <tr>
+            <th class="table-style">Sr. #</th>
+            <th class="table-style">Name</th>
+            <th class="table-style">address</th>
+            <th class="table-style">postcode</th>
+            <?php
+
+            $sql_query = "SELECT * FROM student";
+
+            $result =  mysqli_query($dbs,$sql);
+            if(mysqli_num_rows($result)>0)                                                {
+
+            $counter = 0;
+            while ($row=  mysqli_fetch_array($result))
+            {
+            $counter++;
+            ?>
+        <tr>
+            <td class="table-style"><?php echo $counter; ?></td>
+            <td class="table-style"><?php echo $row['name']; ?></td>
+            <td class="table-style"><?php echo $row['address']; ?></td>
+            <td class="table-style"><?php echo $row['postcode']; ?></td>
+        </tr>
+        <?php
+
+        }//end of for loop
+        }//end if statement ?>
+
+    </table>
+
+
+</main>
+</body>
+</html>
+
+
+
+
+<!--
 
 include "dbConnect.php";
 
@@ -42,4 +126,4 @@ while($row = $result->fetch_array())
     echo "<p>{$diet}</p>";*/
     echo "<br>";
 }
-?>
+?>-->

954281bfd3199898c6d92e1979fb3ef21a51e88b
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 15:56:16 +0000

    new Db connection new viewstudent page

--- dbConnect.php
+++ dbConnect.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER','host=ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME','b3c438583f3e44');
 define('DB_PASSWORD','3cf27899');
 define('DB_DATABASE','friendshiplink');

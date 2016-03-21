780267bbc7e05f82f2469f13611e1130f1fe8a98
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 15:12:37 +0000

    new Db connection new viewstudent page

--- view_student.php
+++ view_student.php
@@ -1,7 +1,51 @@
-<?php
+<?include "dbConnect.php";?>
 
-include "dbConnect.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
+</head>
+<body>
+<header>
+    <div id ="logo">
+        <a href="index.html"><img src="images/fi-logo.png" alt="Demo" width="200"></a>
+    </div>
 
+    <h1><a href="index.html">FriendshipLink WebApp</a></h1>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li class="dropdown">
+                <a class="active dropbtn" href="#">New</a>
+                <div class="dropdown-content">
+                    <a href="new_student.php">New Student Details</a>
+                    <a href="new_host.php">New Host Details</a>
+                </div>
+            </li>
+            <li  class="dropdown">
+                <a href="#" class="dropbtn">View</a>
+                <div class="dropdown-content">
+                    <a href="view_student.php">View Students </a>
+                    <a href="#">View Hosts</a>
+                    <a href="#">View Match</a>
+                </div>
+            </li>
+            <li><a href="#">Create Match</a></li>
+            <li><a href="#">Generate Report</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <h2>Student List</h2>
+    <ul>
+
+<?
 $sql_query = "SELECT * FROM student";
 $result = $db->query($sql_query);
 while($row = $result->fetch_array())
@@ -38,7 +82,9 @@ while($row = $result->fetch_array())
     echo "<p>{$interests}</p>";
     echo "<p>{$diet}</p>";
     echo "<br>";
-
 }
-
 ?>
+   </ul>
+    </main>
+</body>
+</html>

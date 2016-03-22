c346aa2a4f999b7caaaa45088dd8240ce7ba0af1
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-22 12:28:54 +0000

    View Host Table

--- view_host.php
+++ view_host.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>View Students</title>
+    <title>View Hosts</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
 </head>
@@ -41,37 +41,37 @@
     </nav>
 </header>
 <main>
-<h2>Student Table</h2>
+    <h2>Host Table</h2>
 
     <table style="margin-left: auto; margin-right: auto;">
         <tr>
-            <th class="table-style">S_ID</th>
+            <th class="table-style">H_ID</th>
             <th class="table-style">Name</th>
             <th class="table-style">Address</th>
             <th class="table-style">PostCode</th>
             <th class="table-style">Telephone Number</th>
             <th class="table-style">E-mail Address</th>
-            <th class="table-style">Home Country</th>
-            <th class="table-style">Age:</th>
-            <th class="table-style">Gender:</th>
             <th class="table-style">Marital Status</th>
-            <th class="table-style">Married Details</th>
-            <th class="table-style">University</th>
-            <th class="table-style">Course of Study</th>
-            <th class="table-style">End of Study Date</th>
-            <th class="table-style">Special Interests</th>
-            <th class="table-style">Special Diet</th>
+            <th class="table-style">No. Children</th>
+            <th class="table-style">Vegeterian Provision</th>
+            <th class="table-style">Student Prefrence</th>
+            <th class="table-style">Church Attended</th>
+            <th class="table-style">Name of Pastor</th>
+            <th class="table-style">Special Interests/th>
+            <th class="table-style">Interested Nation</th>
+            <th class="table-style">Other Information</th>
 
-        <?php
-            $sql_query = "SELECT * FROM student";
+
+            <?php
+            $sql_query = "SELECT * FROM host";
             $result =  $dbs->query($sql_query);
 
             if(mysqli_num_rows($result)>0){
-                $counter = 0;
+            $counter = 0;
 
             while ($row = $result->fetch_array())
             {
-                $counter++;
+            $counter++;
             ?>
         <tr>
             <td><?php echo $counter;?></td>
@@ -80,24 +80,23 @@
             <td><?php echo $row['postcode'];?></td>
             <td><?php echo $row['phoneNbr'];?></td>
             <td><?php echo $row['email'];?></td>
-            <td><?php echo $row['nationality'];?></td>
-            <td><?php echo $row['age'];?></td>
-            <td><?php echo $row['gender'];?></td>
             <td><?php echo $row['status'];?></td>
-            <td><?php echo $row['ifMarried'];?></td>
-            <td><?php echo $row['university'];?></td>
-            <td><?php echo $row['course'];?></td>
-            <td><?php echo $row['endOfStudy'];?></td>
+            <td><?php echo $row['children'];?></td>
+            <td><?php echo $row['vegan'];?></td>
+            <td><?php echo $row['preference'];?></td>
+            <td><?php echo $row['church'];?></td>
+            <td><?php echo $row['pastor'];?></td>
             <td><?php echo $row['interests'];?></td>
-            <td><?php echo $row['diet'];?></td>
+            <td><?php echo $row['interest_nationality'];?></td>
+            <td><?php echo $row['comments'];?></td>
         </tr>
         <?php
-            }
+        }
         }
         $result->close();
         $dbs->close();
         ?>
-</table>
+    </table>
 </main>
 
 <footer>

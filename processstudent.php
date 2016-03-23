ea62376193832bbd74225541bfc2008bedfa0cef
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-23 14:03:29 +0000

    Edited End of study format

--- processstudent.php
+++ processstudent.php
@@ -27,7 +27,7 @@ if(submit){
 
     try {
         $sql = "INSERT INTO student (name, address,postcode,phoneNbr,email,nationality,age,gender,status,ifMarried,university,course,endOfStudy,interests,diet)
-                VALUES ('$Name', '$Address', '$Postcode',$phoneNbr,'$email', '$nationality', $age, '$gender','$Status', '$ifMarried', '$university', '$course',$endOfStudy,'$interests','$diet')";
+                VALUES ('$Name', '$Address', '$Postcode',$phoneNbr,'$email', '$nationality', $age, '$gender','$Status', '$ifMarried', '$university', '$course','$endOfStudy','$interests','$diet')";
         $sth = $dbs->query($sql);
     } catch(PDOException $e) {
         echo $e->getMessage();

8f3188d76bc3114590422501b5d65a7379e075b6
somtoeluwa <1412632@rgu.ac.uk>
2016-04-11 14:35:59 +0100

    create first table

--- create_match.php
+++ create_match.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.js"></script>
     <title>Create Matches</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
@@ -40,7 +42,56 @@
         </ul>
     </nav>
 </header>
+<!-- end of Header -->
 
+
+<!-- Start of Main-->
+<main>
+
+    <div id="studenttable">
+
+        <script>$(document).ready( function () {
+        $('#table_id').DataTable();
+        } );</script>
+
+        <table id="table_id" class="display">
+            <thead>
+            <tr>
+                <th>S_ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Country</th>
+                <th>Diet</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+                <td>Row 1 Data 3</td>
+                <td>Row 1 Data 4</td>
+                <td>Row 1 Data 5</td>
+                <td>Row 1 Data 6</td>
+            </tr>
+            <tr>
+                <td>Row 2 Data 1</td>
+                <td>Row 2 Data 2</td>
+                <td>Row 2 Data 3</td>
+                <td>Row 2 Data 4</td>
+                <td>Row 2 Data 5</td>
+                <td>Row 2 Data 6</td>
+            </tr>
+
+            </tbody>
+        </table>
+
+    </div>
+
+
+
+</main>
+<!-- End of Main-->
 
 
 <footer>

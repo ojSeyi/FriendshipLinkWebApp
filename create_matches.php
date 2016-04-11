e1f0e3563abf5e776f4948cb7004dafd6c8aa42e
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-11 22:06:48 +0100

    Making the match work

--- create_matches.php
+++ create_matches.php
@@ -76,7 +76,7 @@
                     ?>
                     <tr>
                         <td><input type="checkbox" name="studentSelected" id="studentSelected" value="<?php
-                            echo $row["h_id"]; ?>" onchange="alert('disabled');" /></td>
+                            echo $row["h_id"]; ?>" onchange="div.getElementById('studentSelected').disabled = 'true';" /></td>
                         <td><?php echo $counter;?></td>
                         <td><?php echo $row['name'];?></td>
                         <td><?php echo $row['preference'];?></td>

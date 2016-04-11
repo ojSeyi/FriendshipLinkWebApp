f33b3f8bf190521ce56a4a6568faaf5df0b13678
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-11 22:36:30 +0100

    creating the processmatch page

---
+++ processmatch.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 4/11/2016
+ * Time: 10:34 PM
+ */
+$getHostId = $_POST['h_id'];
+$getStudentId = $_POST['S_ID'];
+
+
+echo 'Host Id:' . $getHostId. ' ' . '<br>'. 'Student Id:' .$getStudentId;
+
+?>

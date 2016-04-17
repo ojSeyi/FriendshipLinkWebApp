e2873a5064f7307dc4443e1fe76f0e2129640419
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-17 14:02:56 +0100

    testing SQL

--- testPage.php
+++ testPage.php
@@ -10,17 +10,35 @@
 
 
 
-$sql = "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID = 1";
-$result2= $dbs->query($sql);
-while( $row2 = $result2->fetch_array()) {
+$sql_query = "SELECT host.* FROM host, `match` where host.h_id = `match`.h_id Group by `match`.h_id;";
+$result =  $dbs->query($sql_query);
+
+while ($row = $result->fetch_array()) {
+    $counter++;
+
+    echo $counter;
+    echo $row['name'];
+    echo $row['preference'];
+    echo $row['interests'];
+    echo $row['interest_nationality'];
+    echo $row['vegan'];
+   echo "<br><br><br><br><br>";
+
+
+
+
+$sql = "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID =" + $row['h_id'] + ";";
+$result2 = $dbs->query($sql);
+while ($row2 = $result2->fetch_array()) {
 
     echo $row2['name'];
     echo $row2['age'];
     echo $row2['gender'];
     echo $row2['nationality'];
     echo $row2['diet'];
+    echo "<br><br>";
 }
-
+}
 
 
 ?>

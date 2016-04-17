e3308e776814853faad1da3c9a4107620bd63232
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-17 02:48:49 +0100

    View match sql test 6

--- view_match.php
+++ view_match.php
@@ -72,7 +72,7 @@
         if(mysqli_num_rows($result)>0){
         $counter = 0;
 
-        foreach ($row = $result->fetch_array() as $hostid )
+        while ($row = $result->fetch_array())
         {
         $counter++;
            // $sql = "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID = " + $row['h_id'] + ";";
@@ -82,14 +82,28 @@
 
         <tr>
             <td class="btn"><?php echo $counter;?></td>
-            <td><?php echo $hostid['name'];?></td>
-            <td><?php echo $hostid['preference'];?></td>
-            <td><?php echo $hostid['interests'];?></td>
-            <td><?php echo $hostid['interest_nationality'];?></td>
+            <td><?php echo $row['name'];?></td>
+            <td><?php echo $row['preference'];?></td>
+            <td><?php echo $row['interests'];?></td>
+            <td><?php echo $row['interest_nationality'];?></td>
             <td><?php echo $row['vegan'];?></td>
         </tr>
-            <tr><?php
-                $sql = "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID = " + $row['h_id'] + ";";
+            </tbody>
+        </table>
+            <table>
+                <tr>
+                <th class="expand1"></th>
+                <th class="expand1">Name</th>
+                <th class="expand1">Age</th>
+                <th class="expand1">Gender</th>
+                <th class="expand1">Country</th>
+                <th class="expand1">Diet</th>
+                </tr>
+
+                <tr><?php
+                    foreach($row as $hostid){
+
+                $sql = "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID = $hostid";
                 $result2= $dbs->query($sql);
                 while( $row2 = $result2->fetch_array()){
                 ?>
@@ -100,11 +114,13 @@
             <td class="expand1"><?php echo $row2['diet'];?></td>
         </tr>
         <?php
-
+        }
             }
 
         }
         }
+            $result->close();
+        $dbs->close();
         ?>
     </table>
 

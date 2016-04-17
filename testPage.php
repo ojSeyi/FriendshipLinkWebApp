b051dfd8ee70ad90cd1d3d064e8aa4720f02b2d8
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-17 14:58:19 +0100

    testing SQL

--- testPage.php
+++ testPage.php
@@ -8,10 +8,47 @@
  * Time: 1:51 PM
  */
 
+$sql = "SELECT host.* FROM host, `match` where host.h_id = `match`.h_id Group by `match`.h_id;";
+$sql .= "SELECT student.* FROM student inner Join `match` on student.S_ID = `match`.S_ID  WHERE `match`.h_ID = " + $row['h_id'] + ";";
 
+// Execute multi query
+if (mysqli_multi_query($dbs,$sql))
+{
+    do
+    {
+        // Store first result set
+        if ($result=mysqli_store_result($dbs)) {
+            // Fetch one and one row
+            while ($row=mysqli_fetch_row($result))
+            {
+                echo $counter;
+                echo $row['host.name'];
+                echo $row['host.preference'];
+                echo $row['host.interests'];
+                echo $row['host.interest_nationality'];
+                echo $row['host.vegan'];
+                echo "<br><br><br><br><br>";
+                echo $row['student.name'];
+                echo $row['student.age'];
+                echo $row['student.gender'];
+                echo $row['student.nationality'];
+                echo $row['student.diet'];
+                echo "<br><br>";
 
+            }
+            // Free result set
+            mysqli_free_result($result);
+        }
+    }
+    while (mysqli_next_result($dbs));
+}
+
+mysqli_close($dbs);
+?>
+<!--
 $sql_query = "SELECT host.* FROM host, `match` where host.h_id = `match`.h_id Group by `match`.h_id;";
-$result =  $dbs->query($sql_query);
+
+$result =  $dbs->
 
 while ($row = $result->fetch_array()) {
     $counter++;
@@ -57,4 +94,4 @@ while ($row2 = $result2->fetch_array()) {
 
 
 
-?>
+?>-->

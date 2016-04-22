4970cbc34f639ebb9c365a3d8d7cb6cdc6ee65fa
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-22 17:39:08 +0100

    test edit

--- Somto/edit_host.php
+++ Somto/edit_host.php
@@ -4,7 +4,7 @@ if(isset($_GET['name']))
 {
     $user_login=$_GET['name'];
     $sql_query = "select * from host where name ='$user_login'";
-    $result = $dbs -> query($sql_query);
+    $result = $dbs->query($sql_query);
     $row = $result->fetch_assoc();
 
     echo $row['email'];

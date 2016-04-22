e78147b3e6596d83c5da701a3392878a5d6386fe
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-22 18:34:20 +0100

    Process host

--- Somto/process_edit_host.php
+++ Somto/process_edit_host.php
@@ -27,8 +27,21 @@ if(isset($_POST['update'])) {
     $comments = $_POST['comments'];
 
 
-    $sql = "UPDATE host set (name, address,postcode,phoneNbr,email,status,children,vegan,preference,church,pastor,interests,interest_nationality,comments)
-        VALUES ('$Name', '$Address', '$Postcode', '$phoneNbr','$Email', '$Status', $Children, '$vegan','$preference', '$Church', '$pastor', '$interests','$interests_nation','$comments')";
+    $sql = "UPDATE host
+            SET name ='$Name',
+            address ='$Address' ,
+            postcode ='$Postcode',
+            phoneNbr = '$phoneNbr',
+            email='$Email',
+            status = '$Status',
+            children = $Children,
+            vegan = '$vegan',
+            preference = '$preference',
+            church ='$Church',
+            pastor = '$pastor',
+            interests = '$interests',
+            interest_nationality = '$interests_nation' ,
+            comments = '$comments'";
 
     if ($sth = $dbs->query($sql)) {
 

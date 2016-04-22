4f3cd6885c688d5c3667a7581ae261d91ba08679
waleokare <waleokare@outlook.com>
2016-04-22 19:04:19 +0100

    home page

--- Wale/index.php
+++ Wale/index.php
@@ -19,16 +19,19 @@ include ("dbConnect.php");
             <input type="password" placeholder="password"/>
             <input type="text" placeholder="email address"/>
             <button>create</button>
-            <p class="message">Already registered? <a href="#">Sign In</a></p>
+            <p class="message">Already registered? <a href="login.php">Sign In</a></p>
         </form>
         <form class="login-form">
             <input type="text" placeholder="username"/>
             <input type="password" placeholder="password"/>
             <button>login</button>
-            <p class="message">Not registered? <a href="#"> create an account</a></p>
+
         </form>
     </div>
 </div>
+
+
+
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
 <script src="js/index.js"></script>

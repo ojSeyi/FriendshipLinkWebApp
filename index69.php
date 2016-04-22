bdd62be37314e65b14501c7290f0c05b83ed36b7
waleokare <waleokare@outlook.com>
2016-04-22 14:21:55 +0100

    testing

--- index69.php
+++ index69.php
@@ -1,3 +1,21 @@
 <?php
+include ("dbConnect.php");
+$dbSuccess = false;
+$dbConnected = mysqli_connect($dbs['hostname'],$dbs['DB_USERNAME'], $dbs['DB_PASSWORD']);
+
+
+if($dbConnected) {
+    $dbSelected = mysqli_select_db($dbConnected, $dbs['database']);
+
+    if ($dbSelected) {
+
+        $dbSuccess = true;
+    } else {
+        echo "DB Selection FAiled";
+    }
+}else{
+    echo "MYSQL Connection FAiled";
+}
+
 
 echo 'working';

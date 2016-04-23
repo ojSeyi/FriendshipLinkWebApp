5a22af13b97f7af06166f61bd1693ddc0ac04ef4
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-23 18:43:46 +0100

    Delete Student test

---
+++ Somto/delete_student.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 4/23/2016
+ * Time: 6:32 PM
+ */
+
+
+include "dbConnect.php";
+
+
+
+$deleteQuery = "DELETE FROM students WHERE S_ID = {$_GET['S_ID']} ";
+$result = $dbs->query($deleteQuery);
+
+if($sth = $dbs->query($sql)){
+
+    header('Location:view_student.php?d=1');
+
+}else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($dbs);
+    header('Location:view_student.php?f=1');
+}
+

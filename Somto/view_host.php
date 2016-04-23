01e3d5b62041162166863f45181071b73cfabc40
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-23 19:21:40 +0100

    Delete Student test

--- Somto/view_host.php
+++ Somto/view_host.php
@@ -54,6 +54,7 @@
             <?php if($_GET['s']) {
                 echo '<span style="color: blue;">Record Edited! </span>';
             }elseif ($_GET['f']) {echo'<span style="color: blue;"> Record Not Edited! </span>';}
+            elseif ($_GET['d']){echo "<span style='color: blue;'> Record Deleted!</span>";}
             ?>
 
         </span>
